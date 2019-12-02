2fcf75d5d7fbdefb394b7527d5c5fd83e209681e
Moses Palacios <mospalacios@gmail.com>
2019-12-01 19:29:12 -0500

    set up HTML page and added 5 questions to questions.js

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+{
+    title: "Which of the following is not true about JavaScript",
+    choices: ["JavaScript is one of three primary languages used in web development", "JavaScript can be used to dynamically change the DOM on a webpage", "JavaScript can do your laundry"],
+    answer: "JavaScript can do your laundry"
+},
+{
+    title: "What does 'bubbling up' refer to?",
+    choices: ["When a JavaScript object has had a big lunch", "The order in which event handlers are called when elements are nested within each other", "When event handlers take priority accorind the size of the element"],
+    answer: "The order in which event handlers are called when elements are nested within each other"
+},
+{
+    title: "Which property is used to stop event handlers from bubbling up?",
+    choices: ["event.stopPropagation()", "event.stopCompilation()", "event.quitBubble()"],
+    answer: "event.stopPropagation()"
+},
+{
+    title: "Why is event delegation useful?",
+    choices: ["To distribute work to other events", "To make your code more readable", "To avoid creating an event handler to multiple children of a single parent element"],
+    answer: "To avoid creating an event handler to multiple children of a single parent element"
+},
+{
+    title: "Why is the effect of event.preventDefault()?",
+    choices: ["Cancels the default behavior of an element","Prevents your CSS code from working", "Keeps your code from being basic"],
+    answer: "Cancels the default behavior of an element"
+},
+]
